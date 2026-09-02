ec80e658be9e3fcfbf1989d1abc88b0d68d87f91
Abhi <kondruabhishek66@gmail.com>
2026-09-02 17:45:28 +0530

    ✅ UWCOI20A - Rating 650 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-02

--- codechef/easy/UWCOI20A---Rating-650/solution.java
+++ codechef/easy/UWCOI20A---Rating-650/solution.java
@@ -2,24 +2,57 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		   Scanner SC=new Scanner(System.in);
-	       int T=SC.nextInt();
-	       int max=0;
-	       for(int i=0;i<T;i++){
-	           int N=SC.nextInt();
-	           for(int j=0;j<N;j++){
-	               int val=SC.nextInt();
-	               if(val>max){
-	                   max=val;
-	               }
-	           }
-	       }
-	       System.out.println(max);
+import java.io.*;
+import java.util.*;
 
-	}
+class Codechef {
+
+    static class FastScanner {
+
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        FastScanner fs = new FastScanner();
+
+        int T = fs.nextInt();
+
+        for (int i = 0; i < T; i++) {
+
+            int N = fs.nextInt();
+
+            long max = 0;
+
+            for (int j = 0; j < N; j++) {
+
+                long val = fs.nextLong();
+
+                if (val > max) {
+                    max = val;
+                }
+            }
+
+            System.out.println(max);
+        }
+    }
 }
